4f1e2702718e26e38f777d39b203855365b9a9a4
Ayodele Oniosun <heywire4u@gmail.com>
2022-07-16 21:44:40 +0100

    feat: create requests

---
+++ app/Http/Requests/Users/UpdatePasswordRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests\Users;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class UpdatePasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'current_password'          => 'required|string',
+            'new_password'              => ['required', 'confirmed', Password::min(8)->uncompromised()],
+            'new_password_confirmation' => 'required|string',
+        ];
+    }
+
+    /**
+     * Custom message for validation
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'current_password.password' => 'Current password is incorrect',
+        ];
+    }
+}
