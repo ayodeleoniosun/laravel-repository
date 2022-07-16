8ad3cadb1ada1aef317713973ffaf6c5db3909a1
Ayodele Oniosun <heywire4u@gmail.com>
2022-07-16 21:23:19 +0100

    feat: create migrations

---
+++ database/migrations/2022_07_16_201805_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('slug')->unique();
+            $table->string('email_address')->unique();
+            $table->string('phone_number');
+            $table->string('password');
+            $table->string('remember_token')->nullable();
+            $table->dateTime('email_verified_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
