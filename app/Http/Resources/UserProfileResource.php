d63f3c4bf3a16e40405672e83bebb80142734e9a
Ayodele Oniosun <heywire4u@gmail.com>
2022-07-16 21:45:33 +0100

    feat: create resources

---
+++ app/Http/Resources/UserProfileResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserProfileResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'         => $this->id,
+            'state'      => $this->state,
+            'city'       => $this->city,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'              => $this->id,
+            'first_name'      => ucfirst($this->first_name),
+            'last_name'       => ucfirst($this->last_name),
+            'fullname'        => $this->fullname,
+            'slug'            => $this->slug,
+            'email_address'   => $this->email_address,
+            'phone_number'    => $this->phone_number,
+            'verified'        => $this->hasVerifiedEmail(),
+            'profile'         => new UserProfileResource($this->whenLoaded('profile')),
+            'profile_picture' => $this->whenLoaded('pictures', function () {
+                return new UserProfilePictureResource($this->pictures->last());
+            }),
+            'created_at'      => $this->created_at,
+            'updated_at'      => $this->updated_at,
+        ];
+    }
+}
