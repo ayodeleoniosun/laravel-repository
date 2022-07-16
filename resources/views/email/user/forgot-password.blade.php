276310b6a827e50b9a7967ec03c7bb7292e330d6
Ayodele Oniosun <heywire4u@gmail.com>
2022-07-16 23:24:17 +0100

    chore: update forgot password mail content

--- resources/views/email/user/forgot-password.blade.php
+++ resources/views/email/user/forgot-password.blade.php
@@ -5,15 +5,17 @@
     <p> We received a request to reset the password for your account. </p>
     <p> Click the button below to reset your password </p>
 
-    <p><a href="{{ $url }}">
+    <p>
+        <a href="{{ $url }}">
             <button class="btn btn-primary"> Reset Password</button>
-        </a></p>
+        </a>
+    </p>
 
     <p> or copy and paste the URL into your browser </p>
 
     <p><a href="{{ $url }}"> {{ $url }} </a></p><br/>
 
-    <p> Ensure that the password is reset within the next ten minutes, else, the link becomes invalid. </p>
+    <p> Ensure that the password is reset within the next thirty minutes, else, the link becomes invalid. </p>
 
     Thanks.
 @endsection
